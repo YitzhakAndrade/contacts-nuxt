aee9eeda786d66d3228ccf934e4bc93631d22e37
Yitzhak Andrade <yitzhakandrade@gmail.com>
2018-05-24 17:22:13 -0300

    routerbase

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,9 @@ const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
 } : {}
 
 module.exports = {
-  ...routerBase,
+  router: {
+    base: '/contacts-nuxt/dist/'
+  },
   modules: [ 
     '@nuxtjs/axios'
   ],
